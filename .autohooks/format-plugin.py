6801a703c177e160565f124d72c21a8c9b617ee0
DenisioMytnysiano <den.mytnyk@ua.fm>
2021-04-19 00:28:05 +0300

    Hook for configured

--- .autohooks/format-plugin.py
+++ .autohooks/format-plugin.py
@@ -1,25 +1,32 @@
 import subprocess
-from autohooks.api import ok, error
-from autohooks.api.git import get_staged_status, stage_files_from_status_list, stash_unstaged_changes
-from autohooks.api.path import match
-from typing import NoReturn, List
+from autohooks.api import ok
+from autohooks.api.git import get_staged_status, stash_unstaged_changes
+from typing import List
 
-python_files_extension = "*.py"
-jupyter_notebooks_extension = "*.ipynb" 
+python_files_extension = ".py"
+jupyter_notebooks_extension = ".ipynb"
+
 
 def get_files_from_commit(extention: str) -> List[str]:
-    return [f for f in get_staged_status() if match(f.path, extention)]
+    return [f for f in get_staged_status()]
+
 
 def precommit(**kwargs):
     files = get_files_from_commit(python_files_extension)
     if not files:
         return 0
-    
+
     with stash_unstaged_changes(files):
         for file in files:
-            subprocess.run(["flake8", str(file)], check = True)
-            ok("File {} formatted successfully".format(str(file)))
-    return 0   
-
-
-    
+            filename = str(file).split()[1]
+            if filename.endswith(python_files_extension):
+                subprocess.run(["flake8", filename], check=True)
+                ok("File {} formatted with flake8".format(filename))
+                subprocess.run(["black", filename], check=True)
+                ok("File {} linted with black".format(filename))
+            elif filename.endswith(jupyter_notebooks_extension):
+                subprocess.run(["flake8-nb", filename], check=True)
+                ok("Notebook {} formatted with flake8".format(filename))
+                subprocess.run(["jblack", filename], check=True)
+                ok("Notebook {} linted with black".format(filename))
+    return 0
--- readme.md
+++ readme.md
@@ -13,16 +13,16 @@ This project was created to develop the best model to win the Shopee - Price Mat
 1.First of all clone the repository using the following [link](https://github.com/DenisioMytnysiano/Shopee-2021-Competition) </br>
 2.Open cmd or bash in home directory and install pipenv using following command
 ```bash
-foo@bar:~$ pip install pipenv
+foo@bar:~$ pip install poetry
 ```
 3.Open the project folder, create virtual environment and install dependencies
 ```bash
-foo@bar:~$ pipenv shell
-foo@bar:~$ pipenv install
+foo@bar:~$ poetry shell
+foo@bar:~$ poetry install
 ```
-4.If you need to get your virtual env path to connect to IDE
+4.If you don't want to install developer dependencies
 ```bash
-foo@bar:~$ pipenv --venv
+foo@bar:~$ poetry install --no-dev
 ```
 
 
