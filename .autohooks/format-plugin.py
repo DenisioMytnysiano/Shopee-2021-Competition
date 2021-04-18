0403e36fd054e3ef57e1642d00a7e66ec623c9ae
DenisioMytnysiano <den.mytnyk@ua.fm>
2021-04-18 21:50:56 +0300

    New dependencies added

--- .autohooks/format-plugin.py
+++ .autohooks/format-plugin.py
@@ -10,22 +10,16 @@ jupyter_notebooks_extension = "*.ipynb"
 def get_files_from_commit(extention: str) -> List[str]:
     return [f for f in get_staged_status() if match(f.path, extention)]
 
-def format_files(files: List[str], extension:str) -> NoReturn:
+def precommit(**kwargs):
+    files = get_files_from_commit(python_files_extension)
     if not files:
-        return
+        return 0
     
     with stash_unstaged_changes(files):
         for file in files:
-            if extension == python_files_extension:
-                subprocess.run(["poetry run flake8", str(file)], check = True)
-                ok("File {} formatted successfully".format(str(file)))
-            if extension == jupyter_notebooks_extension:
-                subprocess.run(["poetry run flake8-nb", str(file)], check = True)
-                ok("Notebook {} formatted successfully".format(str(file)))
-        return    
+            subprocess.run(["flake8", str(file)], check = True)
+            ok("File {} formatted successfully".format(str(file)))
+    return 0   
 
-def precommit(**kwargs):
-    python_files = get_files_from_commit(python_files_extension)
-    json_files = get_files_from_commit(jupyter_notebooks_extension)
 
     
