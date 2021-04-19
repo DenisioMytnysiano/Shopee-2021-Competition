50958fe7ea1993d9e0e2df4c06f65c641bdc2801
DenisioMytnysiano <den.mytnyk@ua.fm>
2021-04-19 22:12:56 +0300

    Hooks reconfigured

--- .autohooks/format-plugin.py
+++ .autohooks/format-plugin.py
@@ -1,32 +1,35 @@
 import subprocess
 from autohooks.api import ok
 from autohooks.api.git import get_staged_status, stash_unstaged_changes
-from typing import List
 
-python_files_extension = ".py"
-jupyter_notebooks_extension = ".ipynb"
-
-
-def get_files_from_commit(extention: str) -> List[str]:
-    return [f for f in get_staged_status()]
+PYTHON_FILES_EXTENSION = ".py"
+JUPYTER_NOTEBOOKS_EXTENSION = ".ipynb"
 
 
-def precommit(**kwargs):
-    files = get_files_from_commit(python_files_extension)
-    if not files:
-        return 0
+def get_files_from_commit(extention: str) -> list[str]:
+    return [f for f in get_staged_status() if str(f).endswith(extention)]
 
-    with stash_unstaged_changes(files):
-        for file in files:
-            filename = str(file).split()[1]
-            if filename.endswith(python_files_extension):
-                subprocess.run(["flake8", filename], check=True)
-                ok("File {} formatted with flake8".format(filename))
-                subprocess.run(["black", filename], check=True)
-                ok("File {} linted with black".format(filename))
-            elif filename.endswith(jupyter_notebooks_extension):
-                subprocess.run(["flake8-nb", filename], check=True)
-                ok("Notebook {} formatted with flake8".format(filename))
-                subprocess.run(["jblack", filename], check=True)
-                ok("Notebook {} linted with black".format(filename))
+
+def precommit(**kwargs) -> int:
+    def get_valid_filenames(files: list[str]) -> list[str]:
+        return [str(file).split()[1] for file in files]
+
+    py_files = get_files_from_commit(PYTHON_FILES_EXTENSION)
+    notebooks = get_files_from_commit(JUPYTER_NOTEBOOKS_EXTENSION)
+    if py_files:
+        with stash_unstaged_changes(py_files):
+            filenames = get_valid_filenames(py_files)
+            files_str = ", ".join(filenames)
+            subprocess.run(["flake8", *filenames], check=True)
+            ok(f"Files {files_str} checked with flake8")
+            subprocess.run(["black", *filenames], check=True)
+            ok(f"Files {files_str} linted with black")
+    if notebooks:
+        with stash_unstaged_changes(notebooks):
+            filenames = get_valid_filenames(notebooks)
+            files_str = ",".join(filenames)
+            subprocess.run(["flake8-nb", *filenames], check=True)
+            ok(f"Notebooks {files_str} checked with flake8-nb")
+            subprocess.run(["jblack", *filenames], check=True)
+            ok(f"Notebooks {files_str} linted with jblack")
     return 0
